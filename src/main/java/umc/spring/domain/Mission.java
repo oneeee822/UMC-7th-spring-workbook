877eeb143cb56995d3c727763a5ef54f6de23e3d
OneE <bestoneeee@naver.com>
2024-11-20 22:26:47 +0900

    feat: Week7 워크북 미션 완료

---
+++ src/main/java/umc/spring/domain/Mission.java
@@ -0,0 +1,38 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.mapping.MemberMission;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer reward;
+
+    @Column(nullable = false)
+    private LocalDateTime deadline;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String missionSpec;
+
+    @ManyToOne(fetch = FetchType.LAZY) //이게 왜 ManyToOne?
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
+    private List<MemberMission> memberMissionList = new ArrayList<>();
+}
---
+++ src/main/java/umc/spring/service/TempService/TempCommandServiceImpl.java
@@ -0,0 +1,12 @@
+package umc.spring.service.TempService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+public class TempCommandServiceImpl implements TempQueryService{
+
+    @Override
+    public void CheckFlag(Integer flag) {
+    }
+}
